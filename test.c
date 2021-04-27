24c637c619470748be8c5b32b0cdb98f3b60725e
akilakj1991 <82851043+akilakj1991@users.noreply.github.com>
2021-04-27 16:14:46 +1200

    Added new lines

--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 char name[80];
+int i;
 int main() {
    printf("Hello, World!\n");
    printf("My Name is Akila Jayawardena\n");
    printf("From Sri Lanka and 28 years old\n");
    printf("I am currently live in New Zealand\n");
-   printf("Please say your name:\n");
+   printf("Please say your name?\n");
    scanf("%s", name);
    printf("Your name is %s.", name);
+   printf("How old are you?\n");
    return 0;
 }
