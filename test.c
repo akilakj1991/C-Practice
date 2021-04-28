ed04e0fbc9d192687d56a945c6f8bf5c7506af8e
akilakj1991 <82851043+akilakj1991@users.noreply.github.com>
2021-04-28 14:49:56 +1200

    adding string functions

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 char name[80];
@@ -13,18 +14,18 @@ int main() {
    printf("From Sri Lanka and 28 years old.\n");
    printf("I am currently live in New Zealand.\n");
    printf("Please say your name?\n");
-   scanf("%s", name);
+   gets(name);
    printf("Hello %s.\n", name);
    printf("How old are you?\n");
    scanf("%d", &age);
    printf("Wow I cannot believe you are %d.\nWhere are you from?\n", age);
-   scanf("%s", country);
+   gets(country);
    printf("Wow, %s is a beautiful country.\nWhere were you born?\n", country);
-   scanf("%s", original_country);
+   gets(original_country);
    printf("Wow, %s is also a beautiful country.\nWhere did you study?\n", original_country);
-   scanf("%s", university);
+   gets(university);
    printf("Where is %s located at?\n", university);
-   scanf("%s", uni_add);
+   gets(uni_add);
    printf("Wow that area is nice too.\n");
    return 0;
 }
