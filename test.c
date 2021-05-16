fbf78eed3d463afdb60deba7c4c4930f1d8826da
akilakj1991 <82851043+akilakj1991@users.noreply.github.com>
2021-05-17 08:23:54 +1200

    changed something

--- test.c
+++ test.c
@@ -8,18 +8,19 @@ char original_country1[80], original_country2[10];
 char university1[10], university2[10];
 char uni_add[80];
 int age;
+char temp1, temp2, temp3, temp4;
 
 //int calc(int num1, int num2);
 
 int main() {
    printf("Hello, my Name is Akila Jayawardena and I am from Sri Lanka and 28 years old. I am currently live in New Zealand.\n");
    printf("Please enter your first name and family name?\n");
-   scanf("%s %s", name1, name2);
-   printf("Hello %s.\n", name1);
+   scanf("%s", &temp1);
+   printf("Hello!! %[^\n].", name1);
    printf("How old are you?\n");
    scanf("%d", &age);
    printf("Wow I cannot believe you are %d.\nWhere are you from?\n", age);
-   scanf("%s %s", country1, country2);
+   scanf("%[^\n]", country1);
    if(strlen(country2) == 0)
    {
       printf("Wow, %s is a beautiful country.\nWhere were you born?\n", country1);
@@ -28,7 +29,7 @@ int main() {
    {
       printf("Wow, %s %s is a beautiful country.\nWhere were you born?\n", country1, country2);
    }
-   scanf("%s %s", original_country1, original_country2);
+   scanf("%[^\n]", original_country1);
    if(strlen(original_country2) == 0)
    {
       printf("Wow, %s is a beautiful country too.\nWhich universty did you study?\n", original_country1);
@@ -37,8 +38,8 @@ int main() {
    {
       printf("Wow, %s %s is a beautiful country too.\nWhich university did you study?\n", original_country1, original_country2);
    }
-   scanf("%s %s", university1, university2);
-   printf("Where is %s %s located at?\n", university1, university2);
+   scanf("%[^\n]", university1);
+   printf("Where is %s located at?\n", university1);
    scanf("%s", uni_add);
    printf("Wow that area is nice too.\n");
    return 0;
